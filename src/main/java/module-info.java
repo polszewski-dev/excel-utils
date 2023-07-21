8dbfac628bb386404530e81c432017bde4fa0f1e
Piotr Olszewski <polszewski.dev@gmail.com>
2023-07-21 13:44:59 +0200

    refactor and updating to java 17

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * User: POlszewski
+ * Date: 2023-07-21
+ */
+module excel.utils {
+	requires org.apache.poi.poi;
+
+	exports polszewski.excel.reader;
+	exports polszewski.excel.reader.templates;
+	exports polszewski.excel.writer;
+	exports polszewski.excel.writer.style;
+}
